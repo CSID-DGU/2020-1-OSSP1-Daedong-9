72c613eefc7688c992926ce0cb845b2fc8d35222
HyunJoo Park <47200141+hodu3709@users.noreply.github.com>
2020-06-02 22:29:22 +0900

    Add files via upload

---
+++ User.java
@@ -0,0 +1,46 @@
+package user;
+
+public class User {
+	private String userID;
+	private String userPassword;
+	private String userGender;
+	private String userTemperature;
+	private String userAddress;
+	private String userEmail;
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	public String getUserTemperature() {
+		return userTemperature;
+	}
+	public void setUserTemperature(String userTemperature) {
+		this.userTemperature = userTemperature;
+	}
+	public String getUserAddress() {
+		return userAddress;
+	}
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+}
