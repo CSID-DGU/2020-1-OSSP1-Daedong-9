8bf79e9e1cdfb4633eb30e9d0c26787744fcf8d2
HyunJoo Park <47200141+hodu3709@users.noreply.github.com>
2020-06-22 03:10:11 +0900

    Add files via upload

---
+++ Closet.java
@@ -0,0 +1,27 @@
+package closet;
+
+public class Closet {
+	private String userID; 
+	private int clothCategory;
+	private int clothAvailable;
+ 
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public int getClothCategory() {
+		return clothCategory;
+	}
+	public void setClothCategory(int clothCategory) {
+		this.clothCategory = clothCategory;
+	}
+	public int getClothAvailable() {
+		return clothAvailable;
+	}
+	public void setClothAvailable(int clothAvailable) {
+		this.clothAvailable = clothAvailable;
+	}
+	 
+	}
